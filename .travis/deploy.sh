88d249f80a8398ac5db2fc404ff43b573594400e
WTRMQDev <TetsuwanAtomu@tuta.io>
2019-02-17 20:38:23 +0300

    Make deploy executable

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,7 +4,31 @@ set -e -x
 
 echo "deploy"
 
+# remove left over files from previous steps
+rm -rf build dist
+mkdir dist
 
+python setup.py bdist_wheel
+
+# On linux we want to build `manylinux1` wheels. See:
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+	docker run --rm -v $(pwd):/io ${WHEELBUILDER_IMAGE} /io/.travis/build-linux-wheels.sh
+else
+	# Only build wheels for the non experimental bundled version
+	if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+		python -m pip install wheel
+		python setup.py bdist_wheel
+	fi
+fi
+
+ls -l dist
+
+python -m pip install twine
+
+# Ignore non-existing files in globs
+shopt -s nullglob
+
+twine upload --skip-existing dist/progpow*.{whl,gz} -u "${PYPI_USERNAME}" -p "${PYPI_PASSWORD}"
 
 set +e +x
 
